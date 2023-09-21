2b5b7c18e25d73da04e9a430ef9db1a2f14cfd9b
Ibaad18 <123141138+Ibaad18@users.noreply.github.com>
2023-09-21 15:46:27 -0400

    Sections Outlined

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import SpecialOffers from './SpecialOffers';
+import Services from './Services';
+import SuperQuality from './SuperQuality';
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    SuperQuality,
+    Subscribe,
+    SpecialOffers,
+    Services,
+    CustomerReviews
+}
